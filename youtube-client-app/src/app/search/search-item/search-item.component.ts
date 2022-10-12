a8ac4046636fe2cd92c9410e42f89ce4a9544e75
Yaroslav Guz <ray.zug@gmail.com>
2022-10-12 15:12:44 +0300

    feat: link search-item and search-results

--- youtube-client-app/src/app/search/search-item/search-item.component.ts
+++ youtube-client-app/src/app/search/search-item/search-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SearchItem } from '../search-item.model';
 
 @Component({
   selector: 'app-search-item',
   templateUrl: './search-item.component.html',
-  styleUrls: ['./search-item.component.scss']
+  styleUrls: ['./search-item.component.scss'],
 })
 export class SearchItemComponent implements OnInit {
+  @Input() searchItem: SearchItem | undefined;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
