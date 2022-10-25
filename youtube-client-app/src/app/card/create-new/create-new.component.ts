461e61982e0a4d7413b9776377cc77618815f54d
Yaroslav Guz <ray.zug@gmail.com>
2022-10-25 17:10:19 +0300

    feat: make 'create-new' form interactive

--- youtube-client-app/src/app/card/create-new/create-new.component.ts
+++ youtube-client-app/src/app/card/create-new/create-new.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-new',
   templateUrl: './create-new.component.html',
-  styleUrls: ['./create-new.component.scss']
+  styleUrls: ['./create-new.component.scss'],
 })
 export class CreateNewComponent implements OnInit {
+  form = new FormGroup({
+    title: new FormControl(null, [
+      Validators.required,
+      Validators.maxLength(15),
+    ]),
+    description: new FormControl(null, [Validators.required]),
+    image: new FormControl(null),
+    link: new FormControl(null, [Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.form.reset();
   }
-
 }
