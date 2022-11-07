4e287bc24e342b3b28ad4df2b5341805161fb963
Yaroslav Guz <ray.zug@gmail.com>
2022-11-07 15:37:21 +0200

    feat: add YoutubeAPI response model

---
+++ youtube-client-app/src/app/search/search-response.model.ts
@@ -0,0 +1,68 @@
+export interface SearchResponse {
+  kind: string;
+  etag: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: Item[];
+}
+
+interface Item {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+  statistics: {
+    viewCount: string;
+    likeCount: string;
+    dislikeCount: string;
+    favoriteCount: string;
+    commentCount: string;
+  };
+}
+
+interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: {
+    title: string;
+    description: string;
+  };
+  defaultAudioLanguage: string;
+}
+
+interface Thumbnails {
+  default: {
+    url: string;
+    width: number;
+    height: number;
+  };
+  medium: {
+    url: string;
+    width: number;
+    height: number;
+  };
+  high: {
+    url: string;
+    width: number;
+    height: number;
+  };
+  standard: {
+    url: string;
+    width: number;
+    height: number;
+  };
+  maxres: {
+    url: string;
+    width: number;
+    height: number;
+  };
+}
