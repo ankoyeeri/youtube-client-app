8571e7b7074f897fec5597a2fb6d884247994395
Yaroslav Guz <ray.zug@gmail.com>
2022-11-23 16:27:59 +0200

    fix: remove unused stuff  from 'app' module

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,40 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SearchComponent } from './search/search.component';
-import { SearchResultsComponent } from './search/search-results/search-results.component';
-import { SearchItemComponent } from './search/search-item/search-item.component';
-import { SearchItemDetailsComponent } from './search/search-item-details/search-item-details.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ErrorComponent } from './error/error.component';
-import { LoginComponent } from './card/login/login.component';
-import { CreateNewComponent } from './card/create-new/create-new.component';
-import { RegistrationComponent } from './card/registration/registration.component';
-import { SearchService } from './search/search.service';
 
 import { AppRoutesModule } from './app-routes.module';
-import { ParagraphFormatPipe } from './pipes/paragraph-format.pipe';
-import { SearchForPipe } from './pipes/searchFor.pipe';
+import { CardsModule } from './card/card.module';
+import { SearchModule } from './search/search.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SearchComponent,
-    SearchResultsComponent,
-    SearchItemComponent,
-    SearchItemDetailsComponent,
-    ErrorComponent,
-    LoginComponent,
-    CreateNewComponent,
-    RegistrationComponent,
-    ParagraphFormatPipe,
-    SearchForPipe,
+  declarations: [AppComponent, HeaderComponent, ErrorComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CardsModule,
+    SearchModule,
+    AppRoutesModule,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutesModule, ReactiveFormsModule],
-  providers: [SearchService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
