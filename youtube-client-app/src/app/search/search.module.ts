a74df24c4074404f1aaf520333271a9c286ba8ba
Yaroslav Guz <ray.zug@gmail.com>
2022-11-23 18:30:43 +0200

    feat: add 'video-stats' shared component

--- youtube-client-app/src/app/search/search.module.ts
+++ youtube-client-app/src/app/search/search.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PipesModule } from '../pipes/pipes.module';
+import { VideoStatsComponent } from '../shared/video-stats/video-stats.component';
 import { SearchItemDetailsComponent } from './search-item-details/search-item-details.component';
 import { SearchItemComponent } from './search-item/search-item.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
@@ -12,6 +13,7 @@ import { SearchRoutesModule } from './search-routes.module';
     SearchItemComponent,
     SearchItemDetailsComponent,
     SearchResultsComponent,
+    VideoStatsComponent,
   ],
   imports: [CommonModule, SearchRoutesModule, PipesModule],
 })
