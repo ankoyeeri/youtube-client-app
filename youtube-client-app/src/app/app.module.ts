230e69acf371b975848d87fee809d96fdfa0456b
Yaroslav Guz <ray.zug@gmail.com>
2022-12-10 18:29:05 +0200

    refactor: add 'header' module

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ErrorComponent } from './error/error.component';
 
 import { AppRoutesModule } from './app-routes.module';
 import { CardsModule } from './card/card.module';
 import { SearchModule } from './search/search.module';
+import { HeaderModule } from './header/header.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ErrorComponent],
+  declarations: [AppComponent, ErrorComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
+    HeaderModule,
     CardsModule,
     SearchModule,
     AppRoutesModule,
