a28d5b43a936901f193cb94947dc99abab1d7153
Yaroslav Guz <ray.zug@gmail.com>
2022-12-25 19:06:33 +0200

    refactor: move 'search' service to 'youtube'

--- youtube-client-app/src/app/youtube/services/search.service.ts
+++ youtube-client-app/src/app/youtube/services/search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { SearchItem } from './search-item.model';
+import { SearchItem } from '../models/search-item.model';
 
 @Injectable({
   providedIn: 'root',
