c06c67513dee14fc1002fde8408a8e6e6c934013
Yaroslav Guz <ray.zug@gmail.com>
2022-12-25 18:57:38 +0200

    refactor: rename SearchRoutesModule

--- youtube-client-app/src/app/youtube/youtube-routes.module.ts
+++ youtube-client-app/src/app/youtube/youtube-routes.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { SearchItemDetailsComponent } from './search-item-details/search-item-details.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchItemDetailsComponent } from './components/search-item-details/search-item-details.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 const routes: Routes = [
   { path: 'results', component: SearchResultsComponent },
@@ -13,4 +12,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class SearchRoutesModule {}
+export class YoutubeRoutesModule {}
