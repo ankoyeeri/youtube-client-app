7fe5f8a4fcfe729ee5b1f4e43744e8c215c2124e
Yaroslav Guz <ray.zug@gmail.com>
2022-12-25 19:30:15 +0200

    refactor: move 'login' component  to 'auth' module

--- youtube-client-app/src/app/auth/auth.module.ts
+++ youtube-client-app/src/app/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './components/login/login.component';
 
-@NgModule()
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [SharedModule, ReactiveFormsModule],
+  exports: [LoginComponent],
+})
 export class AuthModule {}
