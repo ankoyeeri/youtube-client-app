15429de29842a5a98e86758b32aad8c86f5e0fbc
Yaroslav Guz <ray.zug@gmail.com>
2022-12-25 19:05:26 +0200

    fix: replace CommonModule by SharedModule

--- youtube-client-app/src/app/youtube/youtube.module.ts
+++ youtube-client-app/src/app/youtube/youtube.module.ts
@@ -1,6 +1,6 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PipesModule } from '../pipes/pipes.module';
+import { SharedModule } from '../shared/shared.module';
 import { SearchItemDetailsComponent } from './components/search-item-details/search-item-details.component';
 import { SearchItemComponent } from './components/search-item/search-item.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
@@ -12,7 +12,7 @@ import { YoutubeRoutesModule } from './youtube-routes.module';
     SearchItemDetailsComponent,
     SearchResultsComponent,
   ],
-  imports: [CommonModule, YoutubeRoutesModule, PipesModule],
+  imports: [SharedModule, YoutubeRoutesModule, PipesModule],
   exports: [
     SearchItemComponent,
     SearchItemDetailsComponent,
