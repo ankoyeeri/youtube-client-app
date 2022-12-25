655d06caf5aebe9221decc406dad7a084a2aa82d
Yaroslav Guz <ray.zug@gmail.com>
2022-12-25 19:10:43 +0200

    refactor: move pipes to 'shared' module

--- youtube-client-app/src/app/shared/pipes/searchFor.pipe.ts
+++ youtube-client-app/src/app/shared/pipes/searchFor.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { SearchItem } from '../search/search-item.model';
+import { SearchItem } from '../youtube/models/search-item.model';
 
 @Pipe({
   name: 'searchFor',
--- youtube-client-app/src/app/shared/shared.module.ts
+++ youtube-client-app/src/app/shared/shared.module.ts
@@ -1,10 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ColorLineComponent } from './color-line/color-line.component';
+import { ParagraphFormatPipe } from './pipes/paragraph-format.pipe';
+import { SearchForPipe } from './pipes/searchFor.pipe';
 import { VideoStatsComponent } from './video-stats/video-stats.component';
 
 @NgModule({
-  declarations: [VideoStatsComponent, ColorLineComponent],
-  exports: [CommonModule, VideoStatsComponent, ColorLineComponent],
+  declarations: [
+    VideoStatsComponent,
+    ColorLineComponent,
+    ParagraphFormatPipe,
+    SearchForPipe,
+  ],
+  exports: [
+    CommonModule,
+    VideoStatsComponent,
+    ColorLineComponent,
+    ParagraphFormatPipe,
+    SearchForPipe,
+  ],
 })
 export class SharedModule {}
