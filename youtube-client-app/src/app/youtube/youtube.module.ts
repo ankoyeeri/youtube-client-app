35a198ccd55734a0f8f4b895a94863b1d7c31e28
Yaroslav Guz <ray.zug@gmail.com>
2022-12-26 18:11:16 +0200

    refactor(youtube): rename routes file

--- youtube-client-app/src/app/youtube/youtube.module.ts
+++ youtube-client-app/src/app/youtube/youtube.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { SearchItemDetailsComponent } from './components/search-item-details/search-item-details.component';
+import { SearchItemDetailsComponent } from './pages/search-item-details/search-item-details.component';
 import { SearchItemComponent } from './components/search-item/search-item.component';
-import { SearchResultsComponent } from './components/search-results/search-results.component';
-import { YoutubeRoutesModule } from './youtube-routes.module';
+import { SearchResultsComponent } from './pages/search-results/search-results.component';
+import { YoutubeRoutesModule } from './youtube.routes.module';
 
 @NgModule({
   declarations: [
--- youtube-client-app/src/app/youtube/youtube.routes.module.ts
+++ youtube-client-app/src/app/youtube/youtube.routes.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../core/guards/auth.guard';
-import { SearchItemDetailsComponent } from './components/search-item-details/search-item-details.component';
-import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { SearchItemDetailsComponent } from './pages/search-item-details/search-item-details.component';
+import { SearchResultsComponent } from './pages/search-results/search-results.component';
 
 const routes: Routes = [
   {
