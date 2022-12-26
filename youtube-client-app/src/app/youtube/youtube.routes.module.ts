725ce151ccd894e2cafbe1971e5a668bc14350d8
Yaroslav Guz <ray.zug@gmail.com>
2022-12-26 18:26:44 +0200

    refactor(youtube): update youtube routing

--- youtube-client-app/src/app/youtube/youtube.routes.module.ts
+++ youtube-client-app/src/app/youtube/youtube.routes.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../core/guards/auth.guard';
+import { HomeComponent } from './pages/home/home.component';
 import { SearchItemDetailsComponent } from './pages/search-item-details/search-item-details.component';
 import { SearchResultsComponent } from './pages/search-results/search-results.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'results',
     canActivate: [AuthGuard],
     component: SearchResultsComponent,
   },
-  { path: ':id', component: SearchItemDetailsComponent },
+  { path: 'results/:id', component: SearchItemDetailsComponent },
 ];
 
 @NgModule({
