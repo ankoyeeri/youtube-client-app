0ecfe70ce77e544ea347e837d538f9fbdba697fa
Yaroslav Guz <ray.zug@gmail.com>
2022-12-27 22:24:31 +0200

    feat(shared): add 'trim' pipe

--- youtube-client-app/src/app/shared/shared.module.ts
+++ youtube-client-app/src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ParagraphFormatPipe } from './pipes/paragraph-format.pipe';
 import { SearchForPipe } from './pipes/searchFor.pipe';
+import { TrimPipe } from './pipes/trim.pipe';
 
 @NgModule({
-  declarations: [ParagraphFormatPipe, SearchForPipe],
-  exports: [CommonModule, ParagraphFormatPipe, SearchForPipe],
+  declarations: [ParagraphFormatPipe, SearchForPipe, TrimPipe],
+  exports: [CommonModule, ParagraphFormatPipe, SearchForPipe, TrimPipe],
 })
 export class SharedModule {}
